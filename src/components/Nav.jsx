0353934b9176990ba556dd8868c57deadc330094
br3nst3rm <adeleke21l4l@gmail.com>
2024-12-23 23:28:46 +0100

    edit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -28,7 +28,7 @@ const Nav = () => {
     <div className="z-50 flex lg:flex-row flex-col justify-between items-center py-[18px] px-[3%] fixed right-0 left-0 bg-black">
       <div className="hidden lg:flex items-center gap-[10px]">
         <p className="text-white font-semibold">#0x33crypt</p>
-        <p className="text-white font-semibold">#WebPro</p>
+        <p className="text-white font-semibold">#WebDevPro</p>
       </div>
 
       <div className=" flex justify-center items-center w-full">
